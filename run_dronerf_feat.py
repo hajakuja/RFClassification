6c6674cc8b50226f5e6bd5c6c5276b4e1cf62062
Uran Cabra <contact@urancabra.com>
2025-10-13 11:32:58 +0200

    Add streaming DroneRF feature extraction pipeline

--- run_dronerf_feat.py
+++ run_dronerf_feat.py
@@ -24,10 +24,13 @@ import importlib
 # Dataset Info
 main_folder = dronerf_raw_path
 t_seg = 20
-Xs_arr, ys_arr, y4s_arr, y10s_arr = load_dronerf_raw(main_folder, t_seg)
 fs = 40e6 #40 MHz
+samples_per_segment = int(t_seg/1e3*fs)
 
-print('length of X:', len(Xs_arr), 'length of y:', len(ys_arr))
+# Streaming configuration for chunk-wise feature extraction
+chunk_size_samples = samples_per_segment * 4  # four segments per read (~memory/perf tradeoff)
+segments_per_batch = 64  # how many segments to accumulate before flushing to disk
+stream_dtype = np.float32
 
 n_per_seg = 1024 # length of each segment (powers of 2)
 n_overlap_spec = 120
@@ -55,6 +58,7 @@ img_spec_folder = "IMG_SPEC_"+high_low+'_'+str(n_per_seg)+"_"+str(t_seg)+"/"
 img_psd_folder = "IMG_PSD_"+high_low+'_'+str(n_per_seg)+"_"+str(t_seg)+"/"
 raw_folder = 'RAW_VOLT_'+str(v_samp_len)+"_"+str(t_seg)+"/" # high and low frequency stacked together
 
+os.makedirs(features_folder, exist_ok=True)
 existing_folders = os.listdir(features_folder)
 
 if high_low == 'H':
@@ -125,67 +129,143 @@ if 'RAW' in feature_to_save:
 if all([not sa_save, not si_save, not pa_save, not pi_save, not raw_save]):
     print('Features Already Exist - Do Not Generate')
 else:
-    n_parts = 24 # process the data in 10 parts so memory doesn't overwhelm
+    print(
+        f'Streaming DroneRF segments with chunk_size_samples={chunk_size_samples} '
+        f'and segments_per_batch={segments_per_batch}'
+    )
 
-    indices_ranges = np.split(np.array(range(len(Xs_arr))), n_parts) 
-    for i in range(n_parts):
-        BILABEL = []
-        DRONELABEL = []
-        MODELALBEL = []
-        F_PSD = []
-        F_SPEC = []
-        F_V = []
-        ir = indices_ranges[i]
-        for j in tqdm(range(len(ir))):
-            d_real = Xs_arr[ir[j]][i_hl]
-            
-            # if save raw data
-            if raw_save:
-                t = np.arange(0, len(d_real))
-                f_high = interpolate.interp1d(t, Xs_arr[ir[j]][0])
-                f_low = interpolate.interp1d(t, Xs_arr[ir[j]][1])
-                tt = np.linspace(0, len(d_real)-1, num=v_samp_len)
+    def flush_batch(batch_idx, BILABEL, DRONELABEL, MODELALBEL, F_SPEC, F_PSD, F_V):
+        if not BILABEL:
+            return BILABEL, DRONELABEL, MODELALBEL, F_SPEC, F_PSD, F_V
 
-                d_v = np.stack((f_high(tt), f_low(tt)), axis=0)
-                F_V.append(d_v)
-            
-            if pa_save or pi_save:
-            # calculate PSD
-                fpsd, Pxx_den = signal.welch(d_real, fs, window=win_type, nperseg=n_per_seg)
-                if pa_save:
-                    F_PSD.append(Pxx_den)
-                if pi_save:
-                    save_psd_image_rf(features_folder, img_psd_folder,
-                                      y10s_arr[ir[j]], i, j, Pxx_den, dim_px, dpi)
-            
-            if sa_save or si_save:
-            # calculate spectrogram
-            # welch's method older
-#           fspec, t, Sxx = signal.spectrogram(d_real, fs, window=win_type, nperseg=n_per_seg)
-            
-                if si_save: # set up fig properties if saving images
-                    plt.clf()
-                    fig,ax = plt.subplots(1, figsize=(dim_px[0]/dpi, dim_px[1]/dpi), dpi=dpi)
-                    fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
-                    ax.axis('tight')
-                    ax.axis('off')
+        if sa_save and F_SPEC:
+            save_array_rf(
+                features_folder + arr_spec_folder,
+                F_SPEC,
+                BILABEL,
+                DRONELABEL,
+                MODELALBEL,
+                'SPEC',
+                n_per_seg,
+                batch_idx,
+            )
 
-                spec, _, _, _ = plt.specgram(d_real, NFFT=n_per_seg, Fs=fs, window=spec_han_window, 
-                                  noverlap=n_overlap_spec, sides='onesided')
-                if si_save:
-                    save_spec_image_fig_rf(features_folder, img_spec_folder, 
-                                           y10s_arr[ir[j]], i, j, fig, dpi)
-                if sa_save:
-                    F_SPEC.append(interpolate_2d(Sxx, (224,224)))
+        if pa_save and F_PSD:
+            save_array_rf(
+                features_folder + arr_psd_folder,
+                F_PSD,
+                BILABEL,
+                DRONELABEL,
+                MODELALBEL,
+                'PSD',
+                n_per_seg,
+                batch_idx,
+            )
 
-            # Labels
-            BILABEL.append(ys_arr[ir[j]])
-            DRONELABEL.append(y4s_arr[ir[j]])
-            MODELALBEL.append(y10s_arr[ir[j]])
-        
-        if sa_save:
-            save_array_rf(features_folder+arr_spec_folder, F_SPEC, BILABEL, DRONELABEL, MODELALBEL, 'SPEC', n_per_seg, i)
-        if pa_save:
-            save_array_rf(features_folder+arr_psd_folder, F_PSD, BILABEL, DRONELABEL, MODELALBEL, 'PSD', n_per_seg, i)
+        if raw_save and F_V:
+            save_array_rf(
+                features_folder + raw_folder,
+                F_V,
+                BILABEL,
+                DRONELABEL,
+                MODELALBEL,
+                'RAW',
+                '',
+                batch_idx,
+            )
+
+        return [], [], [], [], [], []
+
+    BILABEL = []
+    DRONELABEL = []
+    MODELALBEL = []
+    F_PSD = []
+    F_SPEC = []
+    F_V = []
+
+    batch_index = 0
+    count_in_batch = 0
+
+    segment_iterator = iter_dronerf_segments(
+        main_folder,
+        t_seg,
+        chunk_size_samples=chunk_size_samples,
+        dtype=stream_dtype,
+    )
+
+    for record in tqdm(segment_iterator, desc='Generating DroneRF features', unit='seg'):
+        segment = record['segment']
+        d_real = segment[i_hl]
+
         if raw_save:
-            save_array_rf(features_folder+raw_folder, F_V, BILABEL, DRONELABEL, MODELALBEL, 'RAW', '', i)
+            t = np.arange(0, segment.shape[1])
+            f_high = interpolate.interp1d(t, segment[0])
+            f_low = interpolate.interp1d(t, segment[1])
+            tt = np.linspace(0, segment.shape[1] - 1, num=v_samp_len)
+            d_v = np.stack((f_high(tt), f_low(tt)), axis=0).astype(np.float32, copy=False)
+            F_V.append(d_v)
+
+        if pa_save or pi_save:
+            fpsd, Pxx_den = signal.welch(d_real, fs, window=win_type, nperseg=n_per_seg)
+            if pa_save:
+                F_PSD.append(Pxx_den.astype(np.float32, copy=False))
+            if pi_save:
+                save_psd_image_rf(
+                    features_folder,
+                    img_psd_folder,
+                    record['ten_label'],
+                    batch_index,
+                    count_in_batch,
+                    Pxx_den,
+                    dim_px,
+                    dpi,
+                )
+
+        if sa_save or si_save:
+            if si_save:
+                plt.clf()
+                fig, ax = plt.subplots(
+                    1, figsize=(dim_px[0] / dpi, dim_px[1] / dpi), dpi=dpi
+                )
+                fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+                ax.axis('tight')
+                ax.axis('off')
+
+            Sxx, _, _, _ = plt.specgram(
+                d_real,
+                NFFT=n_per_seg,
+                Fs=fs,
+                window=spec_han_window,
+                noverlap=n_overlap_spec,
+                sides='onesided',
+            )
+            if si_save:
+                save_spec_image_fig_rf(
+                    features_folder,
+                    img_spec_folder,
+                    record['ten_label'],
+                    batch_index,
+                    count_in_batch,
+                    fig,
+                    dpi,
+                )
+            if sa_save:
+                F_SPEC.append(interpolate_2d(Sxx, (224, 224)).astype(np.float32, copy=False))
+
+        BILABEL.append(record['bi_label'])
+        DRONELABEL.append(record['four_label'])
+        MODELALBEL.append(record['ten_label'])
+
+        count_in_batch += 1
+
+        if count_in_batch >= segments_per_batch:
+            BILABEL, DRONELABEL, MODELALBEL, F_SPEC, F_PSD, F_V = flush_batch(
+                batch_index, BILABEL, DRONELABEL, MODELALBEL, F_SPEC, F_PSD, F_V
+            )
+            batch_index += 1
+            count_in_batch = 0
+
+    BILABEL, DRONELABEL, MODELALBEL, F_SPEC, F_PSD, F_V = flush_batch(
+        batch_index, BILABEL, DRONELABEL, MODELALBEL, F_SPEC, F_PSD, F_V
+    )
+
