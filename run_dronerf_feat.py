b72b96d81b6324c71b06eae194f42788649ed53b
Uran <contact@urancabra.com>
2025-10-21 11:39:46 +0200

    specify fetaures

--- run_dronerf_feat.py
+++ run_dronerf_feat.py
@@ -36,8 +36,8 @@ n_per_seg = 1024 # length of each segment (powers of 2)
 n_overlap_spec = 120
 win_type = 'hamming' # make ends of each segment match
 high_low = 'L' #'L', 'H' # high or low range of frequency
-feature_to_save = ['PSD'] # what features to generate and save: SPEC or PSD
-format_to_save = ['IMG'] # IMG or ARR or RAW
+feature_to_save = ['PSD', 'SPEC', ] # what features to generate and save: SPEC or PSD
+format_to_save = ['IMG', 'ARR'] # IMG or ARR or RAW
 to_add = True
 spec_han_window = np.hanning(n_per_seg)
 
